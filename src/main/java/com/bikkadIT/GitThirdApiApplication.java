b686954d4b9b2a9b6d3865424f150e3e371453a5
KraziePietro <Kishorlate74@gmail.com>
2022-08-05 10:52:35 +0530

    Project created

---
+++ src/main/java/com/bikkadIT/GitThirdApiApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitThirdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitThirdApiApplication.class, args);
+	}
+
+}
